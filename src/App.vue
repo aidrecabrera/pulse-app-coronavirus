f4a9a1059a54f477edec92e1b6b1a6e3ba984c69
Aidre Cabrera <alscgaming@gmail.com>
2022-04-09 20:37:56 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/IndexMain.vue'
+import IndexMain from './components/IndexMain.vue'
+import TheDigosSummaryCovid from './components/page-cards/main/TheDigosSummaryCovid.vue'
+import TheDigosSummaryVax from './components/page-cards/main/TheDigosSummaryVax.vue'
+import TheDigosAlertLevelBoard from './components/page-cards/main/TheDigosAlertLevelBoard.vue'
+import TheHealthAndWellbeing from './components/page-cards/main/TheHealthAndWellbeing.vue';
+import TheNavigationBar from './components/page-cards/header/TheNavigationBar.vue'
+import TheCovidUpdates from './components/page-cards/main/TheCovidUpdates.vue'
+</script>
+
+<template>
+  <header>
+    <TheNavigationBar />
+  </header>
+  <main>
+    <div class="bg-white text-black">
+      <TheDigosSummaryCovid />
+    </div>
+    <div class="bg-white text-black">
+      <TheDigosSummaryVax />
+    </div>
+    <div class="bg-slate-800">
+      <TheDigosAlertLevelBoard />
+    </div>
+    <div class="bg-white text-black">
+      <TheHealthAndWellbeing />
+    </div>
+    <div class="bg-white text-black">
+      <TheCovidUpdates />
+    </div>
+  </main>
+  <footer></footer>
+</template>
+
+<style>
+@import "./assets/base.css";
+@import "./index.css";
+</style>
